836ac86d0fe84828d802aebb4cd9f5a65dd6999e
Gabriel_venerusso <98406849+zG0ubiezz@users.noreply.github.com>
2023-05-04 13:18:15 -0300

    Add files via upload

---
+++ Trabalho02/js/main.js
@@ -0,0 +1,93 @@
+today = new Date();
+currentMonth = today.getMonth();
+currentYear = today.getFullYear();
+selectYear = document.getElementById("year");
+selectMonth = document.getElementById("month");
+
+months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro", ];
+
+monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+function next() {
+  currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+  currentMonth = (currentMonth + 1) % 12;
+  showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+  currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+  currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+  showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+  currentYear = parseInt(selectYear.value);
+  currentMonth = parseInt(selectMonth.value);
+  showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+  let firstDay = new Date(year, month).getDay();
+
+  tbl = document.getElementById("calendar-body"); 
+  
+  //Calendário
+  tbl.innerHTML = "";
+  document.getElementById("imagem").innerHTML =  "<img src='logo.png' widht='100px' height='100px'></img>";
+  monthAndYear.innerHTML = "Calendário Acadêmico"; 
+  selectYear.value = year;
+  selectMonth.value = month;
+
+  // células
+  let date = 1;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement("tr");
+
+    //células individuais
+    for (let j = 0; j < 7; j++) {
+      if (i === 0 && j < firstDay) {
+        cell = document.createElement("td");
+        cellText = document.createTextNode("");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      } else if (date > daysInMonth(month, year)) {
+        break;
+      } else {
+        cell = document.createElement("td");
+        cellText = document.createTextNode(date);
+        dataDate = document.createAttribute("data-date");
+
+        if (month < 10) {
+          dataDate.value = year + "-0" + (month + 1) + "-" + date;
+        } else {
+          dataDate.value = year + "-" + (month + 1) + "-" + date;
+        }
+
+        cell.setAttributeNode(dataDate);
+        if (
+          date === today.getDate() &&
+          year === today.getFullYear() &&
+          month === today.getMonth()
+        ) {
+          cell.classList.add("today");
+        }
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        date++;
+      }
+    }
+    tbl.appendChild(row);
+
+    fetch('data.JSON')
+      .then(response => response.json())
+      .then(data => {
+        
+      })
+
+  }
+}
+
+function daysInMonth(iMonth, iYear) {
+  return 32 - new Date(iYear, iMonth, 32).getDate();
+}
